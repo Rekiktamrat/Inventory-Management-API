ce0f76dee347b41f1ca4e4d7709af00ba6273710
Rekik Tamrat <rekiktamrat72@gmail.com>
2026-02-12 12:20:24 +0300

    Fix Render deployment: Downgrade Django to 5.1.5 and specify Python 3.12 for stability

--- Backend/build.sh
+++ Backend/build.sh
@@ -6,7 +6,5 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
-# Run migrations in order to avoid dependency issues
-python manage.py migrate contenttypes
-python manage.py migrate auth
-python manage.py migrate
+# Run migrations
+python manage.py migrate --no-input

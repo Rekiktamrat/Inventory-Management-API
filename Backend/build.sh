a0a6fdfbdeec45b3233cb9e8d7621bb56c151bb4
Rekik Tamrat <rekiktamrat72@gmail.com>
2026-02-12 12:12:58 +0300

    Fix migration order in build.sh to resolve auth_user relation error

--- Backend/build.sh
+++ Backend/build.sh
@@ -5,4 +5,8 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+
+# Run migrations in order to avoid dependency issues
+python manage.py migrate contenttypes
+python manage.py migrate auth
 python manage.py migrate
